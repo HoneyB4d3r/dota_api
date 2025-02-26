d8cf6ffbf162a2ad19bad6c54bfe4530a62f1a9e
HoneyB4d3r <badger_honey@web.de>
2025-02-26 10:50:09 +0100

    changes program to request player data

--- main.py
+++ main.py
@@ -4,15 +4,31 @@ import pandas as pd
 
 
 def main():
-    # match_id = "8189523293"
-    # url = f"https://api.opendota.com/api/matches/{match_id}"
-    player_id = "64797907"
-    url = f"https://api.opendota.com/api/matches/{player_id}"
-    response = requests.get(url)
-    print(response.status_code)
+
+    # match_id = 8189523293
+    #
+    # response = api_request("matches", match_id)
+    # print(response.status_code)
+    #
     # match_data = response.json()
+    # print(match_data["players"])
+
+    player_id = 64797907
+
+    response = api_request("players", player_id)
+    print(response.status_code)
+
     player_data = response.json()
-    print(player_data["profile"]["leaderboard_rank"])
+    print(player_data)
+
+
+def api_request(request: str, player_id: int):
+    url = f"https://api.opendota.com/api/{request}/{player_id}"
+    return requests.get(url)
+
+
+def load_json():
+    pass
 
 
 if __name__ == "__main__":
